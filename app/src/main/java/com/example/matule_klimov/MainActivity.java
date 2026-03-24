984e905ea45b9b67e32fce85d1795ea627befa53
ANosok <kirill60278@gmail.com>
2026-03-24 15:54:56 +0500

    Выполненная работа без текстовых полей. Климов

--- app/src/main/java/com/example/matule_klimov/MainActivity.java
+++ app/src/main/java/com/example/matule_klimov/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.uicomponents.button.BthBig;
+import com.example.uicomponents.button.BthCustom;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        BthBig btnPrimary = findViewById(R.id.btnPrimary);
+        BthBig btnEnable = findViewById(R.id.btnEnable);
+        BthBig btnTertiary = findViewById(R.id.btnTertiary);
+        BthBig btnSecondary = findViewById(R.id.btnSecondary);
+
+        btnPrimary.init("Отправить", BthCustom.TypeButton.PRIMARY);
+        btnEnable.setEnabled(false);
+        btnTertiary.init("Авторизоваться", BthCustom.TypeButton.TERTIARY);
+        btnSecondary.init("Забыли пароль?", BthCustom.TypeButton.SECONDARY);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
