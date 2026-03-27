245e8b6d7043ef1808e6a45980805a06d133f74f
Lobanov <lbnv.mkhl@gmail.com>
2026-03-27 12:36:27 +0500

    Блок с паролем. Климов

--- UIComponents/src/main/java/com/example/uicomponents/text/TextPassword.java
+++ UIComponents/src/main/java/com/example/uicomponents/text/TextPassword.java
@@ -7,24 +7,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.uicomponents.R;
-import com.example.uicomponents.button.BthCustom;
 
-public class TextBig extends TextCustom {
-
-    public TextBig(@NonNull Context context) {
+public class TextPassword extends TextCustom{
+    public TextPassword(@NonNull Context context) {
         super(context);
     }
 
-    public TextBig(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public TextPassword(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public TextBig(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public TextPassword(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
-
     @Override
     public void init(Integer idLayout) {
-        super.init(R.layout.text);
+        super.init(R.layout.text_passord);
     }
 }
